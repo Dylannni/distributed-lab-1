fe0e16833047bfbb740bde8d8dab6bfb6a26f3be
Dylan Lin <dylanlin0411@gmail.com>
2024-10-14 22:53:51 +0100

    edited - client server model

--- server/server.go
+++ server/server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
 )
 
 type Message struct {
@@ -15,12 +15,21 @@ type Message struct {
 func handleError(err error) {
 	// TODO: all
 	// Deal with an error event.
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
 
 func acceptConns(ln net.Listener, conns chan net.Conn) {
 	// TODO: all
 	// Continuously accept a network connection from the Listener
 	// and add it to the channel for handling connections.
+	for {
+		conn, err := ln.Accept()
+		handleError(err)
+		conns <- conn
+		fmt.Println("New user connected")
+	}
 }
 
 func handleClient(client net.Conn, clientid int, msgs chan Message) {
@@ -29,6 +38,16 @@ func handleClient(client net.Conn, clientid int, msgs chan Message) {
 	// Read in new messages as delimited by '\n's
 	// Tidy up each message and add it to the messages channel,
 	// recording which client it came from.
+	reader := bufio.NewReader(client)
+	for {
+		// Read in new messages as delimited by '\n'
+		msg, err := reader.ReadString('\n')
+		handleError(err)
+
+		// Tidy up each message and add it to the messages channel
+		// Add it to the messages channel, recording which client it came from
+		msgs <- Message{sender: clientid, message: msg}
+	}
 }
 
 func main() {
@@ -38,6 +57,8 @@ func main() {
 	flag.Parse()
 
 	//TODO Create a Listener for TCP connections on the port given above.
+	ln, err := net.Listen("tcp", *portPtr)
+	handleError(err)
 
 	//Create a channel for connections
 	conns := make(chan net.Conn)
@@ -46,6 +67,8 @@ func main() {
 	//Create a mapping of IDs to connections
 	clients := make(map[int]net.Conn)
 
+	clientID := 0
+
 	//Start accepting connections
 	go acceptConns(ln, conns)
 	for {
@@ -53,11 +76,20 @@ func main() {
 		case conn := <-conns:
 			//TODO Deal with a new connection
 			// - assign a client ID
+			client := conn
 			// - add the client to the clients map
+			clients[clientID] = conn
 			// - start to asynchronously handle messages from this client
+			go handleClient(client, clientID, msgs)
+			clientID++
 		case msg := <-msgs:
 			//TODO Deal with a new message
 			// Send the message to all clients that aren't the sender
+			for i, client := range clients {
+				if msg.sender != i {
+					fmt.Fprintf(client, msg.message)
+				}
+			}
 		}
 	}
 }
